6fd1f797220f745349e9b8089c434d76c29cab9b
Darryl Martin <darryl.martin@gmail.com>
2016-10-02 18:19:07 -0700

    incr

--- tests/LoadTickets_tests.py
+++ tests/LoadTickets_tests.py
@@ -1,22 +1,11 @@
+from LoadTickets import *
 import unittest
-import LoadTickets_tests
-#import work_ticket
-#import os
 
 __author__ = 'Darryl Martin'
 
-def setup():
-    print "SETUP!"
+class FileHandlingTestCase(unittest.TestCase):
 
-def teardown():
-    print "TEAR DOWN!"
-
-def test_basic():
-    print "I RAN!"
-
-class FileHandlingTestCase(unittest.TestCase)
-
-    def _test_system_config(self):
+    def test_system_config(self):
         filename = 'test_config.txt'
         f = open(filename, 'w')
         f.write('JIRA|param1|param2\n')
@@ -24,15 +13,14 @@ class FileHandlingTestCase(unittest.TestCase)
         f.close()
         system_list = load_system_config(filename)
         os.remove(filename)
-        assertEquals (system_list[0].sys_type == 'JIRA')
-        assert (system_list[0].param1 == 'param1')
-        assert (system_list[0].param2 == 'param2')
-        assert (system_list[1].sys_type == 'QTASK')
-        assert (system_list[1].param1 == 'param3')
-        assert (system_list[1].param2 == 'param4')
+        self.assertEqual(system_list[0].sys_type, 'JIRA')
+        self.assertEqual(system_list[0].param1, 'param1')
+        self.assertEqual(system_list[0].param2, 'param2')
+        self.assertEqual(system_list[1].sys_type, 'QTASK')
+        self.assertEqual(system_list[1].param1, 'param3')
+        self.assertEqual(system_list[1].param2, 'param4')
 
-
-    def _test_get_query_criteria(self):
+    def test_get_query_criteria(self):
         filename = 'test_criteria.txt'
         f = open(filename, 'w')
         f.write('JIRA|param1|param2\n')
@@ -40,23 +28,32 @@ class FileHandlingTestCase(unittest.TestCase)
         f.close()
         system_list = load_query_criteria(filename)
         os.remove(filename)
-        assert (system_list[0].sys_type == 'JIRA')
-        assert (system_list[0].param1 == 'param1')
-        assert (system_list[0].param2 == 'param2')
-        assert (system_list[1].sys_type == 'QTASK')
-        assert (system_list[1].param1 == 'param3')
-        assert (system_list[1].param2 == 'param4')
+        self.assertEqual(system_list[0].sys_type, 'JIRA')
+        self.assertEqual(system_list[0].param1, 'param1')
+        self.assertEqual(system_list[0].param2, 'param2')
+        self.assertEqual(system_list[1].sys_type, 'QTASK')
+        self.assertEqual(system_list[1].param1, 'param3')
+        self.assertEqual(system_list[1].param2, 'param4')
 
-class TicketInfoTestCase(unittest.TestCase)
-    def _test_load_tickets(self):
+'''
+class TicketInfoTestCase(unittest.TestCase):
+
+    def test_load_tickets(self):
         assert (0 == 1)
         pass
 
 
-    def _test_display_tickets(self):
+    def test_display_tickets(self):
         assert (0 == 1)
         pass
+'''
 
+
+def setup():
+    print ("SETUP!")
+
+def teardown():
+    print ("TEAR DOWN!")
 
 if __name__ == '__main__':
     unittest.main()
